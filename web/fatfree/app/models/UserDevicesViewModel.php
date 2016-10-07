28b8778ae80e4404687e0439f831c91ee58b4fc1
Drew Budwin <drew@budw.in>
2016-10-06 20:45:18 -0400

    Created function to validate user can perform certain actions on a device
    -Currently only implemented when a user tries to delete a device, it makes sure the user owns the device they are trying to delete.
    -If a user tries to perform an invalid action, the request is ignored and the action method will handle the redirect as if the action was successful, just without performing the CRUD operation
    -This is implemented by utilizing the MySQL view that joins `Users` to `Devices` and a lookup is done in this view for a result that has both the `UserID` and the `DeviceID`

---
+++ web/fatfree/app/models/UserDevicesViewModel.php
@@ -0,0 +1,24 @@
+<?php
+
+class UserDevicesViewModel extends DB\SQL\Mapper {
+    protected $db;
+    protected $userDevicesView;
+
+    public function __construct(DB\SQL $db) {
+        parent::__construct($db, "UserDevicesView");
+        $db = $this->db;
+        $this->userDevicesView = new DB\SQL\Mapper($this->db, "UserDevicesView");
+    }
+
+    function devicesForUser($userId) {
+        $devicesForUser = $this->userDevicesView->find(array("UserDevices_UserID = ?", $userId));
+
+        return $devicesForUser;
+    }
+
+    public function doesUserOwnDevice($userId, $deviceId) {
+        $this->userDevicesView->load(array("UserDevices_UserID = ? AND DeviceID = ?", $userId, $deviceId));
+
+        return !$this->userDevicesView->dry();
+    }
+}
