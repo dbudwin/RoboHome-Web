bc00b18f73af55c7e9340feebb67e6bc2922f082
Drew Budwin <drew@budw.in>
2016-10-13 20:30:30 -0400

    Refactoring of the LoginController
    -Smaller functions
    -Better variable name
    -Removed the `controversial` PHPMD rules since there wasn't a good way around them and this rule doesn't currently allow exceptions.  The `access_token` variable is aptly named so because it is a query parameter generated automatically by Amazon.  There also seems to be a bug with Fast CGI if I were to use `filter_input(INPUT_SERVER, "QUERY_STRING")` it would return `null` and is a known issue with PHP.

--- web/fatfree/app/controllers/LoginController.php
+++ web/fatfree/app/controllers/LoginController.php
@@ -6,13 +6,7 @@ class LoginController extends Controller
 {
     public function beforeRoute()
     {
-        //Overridden to prevent endless redirects if a user is redirected to the login page for not being logged in
-
-        if ($this->f3->get("SESSION.user") !== null) {
-            if ($this->f3->get("ALIAS") == "loginPage") {
-                $this->f3->reroute("@devices");
-            }
-        }
+        $this->redirectLoggedInUserToDevicesPage($this->f3);
     }
 
     public function index()
@@ -25,43 +19,12 @@ class LoginController extends Controller
     {
         parse_str($_SERVER["QUERY_STRING"]);
 
-        //Verify that the access token belongs to us
-        $c = curl_init("https://api.amazon.com/auth/o2/tokeninfo?access_token=" . urlencode($access_token));
-        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-            
-        $r = curl_exec($c);
-        curl_close($c);
-        $d = json_decode($r);
+        $this->verifyUserTokenMatchesAmazonToken($f3, $access_token);
 
-        if ($d->aud != $f3->get("AMAZON_TOKEN")) {
-            //The access token does not belong to us
-            header('HTTP/1.1 404 Not Found');
-            echo 'Page not found';
-            exit;
-        }
-            
-        //Exchange the access token for user profile
-        $c = curl_init("https://api.amazon.com/user/profile");
-        curl_setopt($c, CURLOPT_HTTPHEADER, array("Authorization: bearer " . $access_token));
-        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-            
-        $r = curl_exec($c);
-        curl_close($c);
-        $d = json_decode($r);
+        $decodedUserProfile = $this->exchangeAccessTokenForDecodedUserProfile($access_token);
+        $loggedInUser = $this->getLoggedInUserProfile($decodedUserProfile);
 
-        $db = $this->db;
-
-        $userModel = new \Models\UserModel($db);
-
-        $currentUser = $userModel->findUser($d->user_id)[0];
-
-        if ($currentUser === null) {
-            $userModel->createNewUser($d->name, $d->email, $d->user_id);
-            $currentUser = $userModel->findUser($d->user_id)[0];
-        }
-
-        $f3->set("SESSION.user", $currentUser->UserID);
-
+        $f3->set("SESSION.user", $loggedInUser->UserID);
         $f3->reroute("@devices");
     }
 
@@ -69,5 +32,79 @@ class LoginController extends Controller
     {
         $f3->clear("SESSION.user");
         $f3->reroute("@loginPage");
+    }
+
+    private function redirectLoggedInUserToDevicesPage($f3)
+    {
+        if ($f3->get("SESSION.user") !== null) {
+            if ($f3->get("ALIAS") == "loginPage") {
+                $f3->reroute("@devices");
+            }
+        }
+    }
+
+    private function verifyUserTokenMatchesAmazonToken($f3, $accessToken)
+    {
+        $amazonOAuthUrl = "https://api.amazon.com/auth/o2/tokeninfo?access_token=" . urlencode($accessToken);
+        $amazonUserProfileCurlHandle = curl_init($amazonOAuthUrl);
+        curl_setopt($amazonUserProfileCurlHandle, CURLOPT_RETURNTRANSFER, true);
+            
+        $amazonUserProfileCurlResultJson = curl_exec($amazonUserProfileCurlHandle);
+        curl_close($amazonUserProfileCurlHandle);
+        $decodedUser = json_decode($amazonUserProfileCurlResultJson);
+
+        $userToken = $decodedUser->aud;
+
+        if ($userToken != $f3->get("AMAZON_TOKEN")) {
+            header("HTTP/1.1 404 Not Found");
+            echo "Page not found";
+            exit;
+        }
+    }
+
+    private function exchangeAccessTokenForDecodedUserProfile($accessToken)
+    {
+        $amazonUserProfileCurlHandle = curl_init("https://api.amazon.com/user/profile");
+        $httpHeaders = array("Authorization: bearer " . $accessToken);
+
+        curl_setopt($amazonUserProfileCurlHandle, CURLOPT_HTTPHEADER, $httpHeaders);
+        curl_setopt($amazonUserProfileCurlHandle, CURLOPT_RETURNTRANSFER, true);
+            
+        $amazonUserProfileCurlResultJson = curl_exec($amazonUserProfileCurlHandle);
+        curl_close($amazonUserProfileCurlHandle);
+
+        $decodedUserProfileArray = json_decode($amazonUserProfileCurlResultJson);
+
+        return $decodedUserProfileArray;
+    }
+
+    private function getLoggedInUserProfile($decodedUserProfile)
+    {
+        $db = $this->db;
+
+        $userModel = new \Models\UserModel($db);
+
+        $userId = $decodedUserProfile->user_id;
+
+        $loggedInUser = $userModel->findUser($userId)[0];
+
+        if ($loggedInUser === null) {
+            $loggedInUser = $this->createNewUser($decodedUserProfile);
+        }
+
+        return $loggedInUser;
+    }
+
+    private function createNewUser($decodedUserProfile)
+    {
+        $name = $decodedUserProfile->name;
+        $email = $decodedUserProfile->email;
+        $userId = $decodedUserProfile->user_id;
+        
+        $userModel->createNewUser($name, $email, $userId);
+
+        $newlyCreatedUser = $userModel->findUser($userId)[0];
+
+        return $newlyCreatedUser;
     }
 }
