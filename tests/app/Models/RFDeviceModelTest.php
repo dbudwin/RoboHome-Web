24f00af7b3aa8b51c3155a8359b969699ad0eb14
Drew Budwin <drew@budw.in>
2016-10-30 16:48:16 -0400

    Added unit tests for the RFDeviceModel class

---
+++ tests/app/Models/RFDeviceModelTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Test\Models;
+
+use Models\DevicesModel;
+use Models\RFDeviceModel;
+
+class RFDeviceModelTest extends BaseModelTest
+{
+    private $devicesModel;
+    private $rfDeviceModel;
+    private $onCode;
+    private $offCode;
+    private $pulseLength;
+    private $newDeviceId;
+
+    public function setUp()
+    {
+        $connection = $this->getConnection();
+
+        $this->devicesModel = new DevicesModel($connection);
+        $this->rfDeviceModel = new RFDeviceModel($connection);
+
+        $this->onCode = $this->faker->randomNumber;
+        $this->offCode = $this->faker->randomNumber;
+        $this->pulseLength = $this->faker->randomNumber;
+
+        $this->addDevice();
+
+        $_POST = array('OnCode' => $this->onCode, 'OffCode' => $this->offCode, 'PulseLength' => $this->pulseLength);
+    }
+
+    public function testAdd()
+    {
+        $this->rfDeviceModel->add($this->newDeviceId);
+
+        $this->assertEquals(1, $this->rfDeviceModel->ID);
+        $this->assertEquals($this->onCode, $this->rfDeviceModel->OnCode);
+        $this->assertEquals($this->offCode, $this->rfDeviceModel->OffCode);
+        $this->assertEquals($this->pulseLength, $this->rfDeviceModel->PulseLength);
+    }
+
+    public function testDelete()
+    {
+        $this->rfDeviceModel->add($this->newDeviceId);
+
+        $this->rfDeviceModel->delete($this->newDeviceId);
+
+        $this->assertEquals(0, $this->rfDeviceModel->count(array('DeviceID = ?', $this->newDeviceId)));
+    }
+
+    private function addDevice()
+    {
+        $name = $this->faker->name;
+        $description = $this->faker->text;
+
+        $_POST = array('Name' => $name, 'Description' => $description);
+        
+        $this->newDeviceId = $this->devicesModel->add();
+    }
+}
