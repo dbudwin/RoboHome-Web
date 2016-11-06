f0a9b48eae1fdb472d1f6e266d72fa93506d67ce
Drew Budwin <drew@budw.in>
2016-11-06 14:28:15 -0500

    Cleaned up UserDevicesViewModel class
    -Manually creating the `$userDevicesView` isn't needed, the ORM takes care of doing this automatically

--- app/models/UserDevicesViewModel.php
+++ app/models/UserDevicesViewModel.php
@@ -5,26 +5,24 @@ namespace Models;
 class UserDevicesViewModel extends \DB\SQL\Mapper
 {
     protected $db;
-    protected $userDevicesView;
 
     public function __construct(\DB\SQL $db)
     {
         parent::__construct($db, "UserDevicesView");
         $db = $this->db;
-        $this->userDevicesView = new \DB\SQL\Mapper($this->db, "UserDevicesView");
     }
 
     public function devicesForUser($userId)
     {
-        $devicesForUser = $this->userDevicesView->find(array("UserDevices_UserID = ?", $userId));
+        $devicesForUser = $this->find(array("UserDevices_UserID = ?", $userId));
 
         return $devicesForUser;
     }
 
     public function doesUserOwnDevice($userId, $deviceId)
     {
-        $this->userDevicesView->load(array("UserDevices_UserID = ? AND DeviceID = ?", $userId, $deviceId));
+        $this->load(array("UserDevices_UserID = ? AND DeviceID = ?", $userId, $deviceId));
 
-        return !$this->userDevicesView->dry();
+        return !$this->dry();
     }
 }
