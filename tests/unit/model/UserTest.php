84e10c01cb618f518df2effd839ca58a45933ff7
Drew Budwin <drew@budw.in>
2017-02-23 00:02:24 -0500

    Adding tests for User

--- tests/unit/model/UserTest.php
+++ tests/unit/model/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Model;
 
+use App\Device;
 use App\User;
 
 class UserTest extends ModelTestCase
@@ -19,5 +20,66 @@ class UserTest extends ModelTestCase
         $this->assertEquals($name, $user->name);
         $this->assertEquals($email, $user->email);
         $this->assertEquals($userId, $user->user_id);
+    }
+
+    public function testDevices_GivenNoDevicesExist_ReturnsNoDevices()
+    {
+        $user = $this->createUser();
+
+        $devices = $user->devices();
+
+        $this->assertEquals(0, $devices->getResults()->count());
+    }
+
+    public function testDevices_GivenNoDevicesExist_ReturnsAllTheDevices()
+    {
+        $user = $this->createUser();
+
+        $this->createDevice($user->id);
+        $this->createDevice($user->id);
+        $this->createDevice($user->id);
+
+        $devices = $user->devices()->getResults();
+
+        $this->assertEquals(3, $devices->count());
+    }
+
+    public function testDoesUserOwnDevice_GivenUserDoesNotOwnAnyDevices_ReturnsFalse()
+    {
+        $user = $this->createUser();
+
+        $doesUserOwnDevice = $user->doesUserOwnDevice(self::$faker->randomDigit());
+
+        $this->assertFalse($doesUserOwnDevice);
+    }
+
+    public function testDoesUserOwnDevice_GivenUserOwnsDevice_ReturnsTrue()
+    {
+        $user = $this->createUser();
+
+        $device = new Device();
+        $device = $device->add(self::$faker->word(), self::$faker->sentence(), self::$faker->randomDigit(), $user->id);
+
+        $doesUserOwnDevice = $user->doesUserOwnDevice($device->id);
+
+        $this->assertTrue($doesUserOwnDevice);
+    }
+
+    private function createUser()
+    {
+        $user = new User();
+        $name = self::$faker->name();
+        $email = self::$faker->email();
+        $userId = self::$faker->uuid();
+
+        $user = $user->add($name, $email, $userId);
+
+        return $user;
+    }
+
+    private function createDevice($userId)
+    {
+        $device = new Device();
+        $device->add(self::$faker->word(), self::$faker->sentence(), self::$faker->randomDigit(), $userId);
     }
 }
