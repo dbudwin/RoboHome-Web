5085c00ac1308128d4371e83334b05bceed977aa
Drew Budwin <drew@budw.in>
2017-02-23 21:10:08 -0500

    Adding tests for Device

--- tests/unit/model/DeviceTest.php
+++ tests/unit/model/DeviceTest.php
@@ -26,6 +26,30 @@ class DeviceTest extends ModelTestCase
 
     public function testRFDevice_GivenRFDeviceAddedToDatabase_FoundRFDeviceMatches()
     {
+        $addedDevice = $this->addDeviceToDatabase();
+        $rfDevice = $this->addRFDeviceToDatabase($addedDevice);
+
+        $foundRfDevice = $addedDevice->rfDevice;
+
+        $this->assertEquals($rfDevice->on_code, $foundRfDevice->on_code);
+        $this->assertEquals($rfDevice->off_code, $foundRfDevice->off_code);
+        $this->assertEquals($rfDevice->pulse_length, $foundRfDevice->pulse_length);
+        $this->assertEquals($rfDevice->device_id, $foundRfDevice->device_id);
+    }
+
+    public function testRFDevice_GivenRFDeviceAddedToDatabase_FoundRFDeviceMatches2()
+    {
+        $addedDevice = $this->addDeviceToDatabase();
+        $this->addRFDeviceToDatabase($addedDevice);
+
+        $addedDevice->delete();
+
+        $this->assertEquals(0, Device::count());
+        $this->assertEquals(0, RFDevice::count());
+    }
+
+    private function addDeviceToDatabase()
+    {
         $device = new Device();
         $name = self::$faker->word();
         $description = self::$faker->sentence();
@@ -34,19 +58,19 @@ class DeviceTest extends ModelTestCase
 
         $addedDevice = $device->add($name, $description, $type, $userId);
 
+        return $addedDevice;
+    }
+
+    private function addRFDeviceToDatabase(Device $device)
+    {
         $rfDevice = new RFDevice();
         $onCode = self::$faker->randomNumber();
         $offCode = self::$faker->randomNumber();
         $pulseLength = self::$faker->randomNumber();
-        $deviceId = $addedDevice->id;
+        $deviceId = $device->id;
 
         $rfDevice = $rfDevice->add($onCode, $offCode, $pulseLength, $deviceId);
 
-        $foundRfDevice = $addedDevice->rfDevice;
-
-        $this->assertEquals($rfDevice->on_code, $foundRfDevice->on_code);
-        $this->assertEquals($rfDevice->off_code, $foundRfDevice->off_code);
-        $this->assertEquals($rfDevice->pulse_length, $foundRfDevice->pulse_length);
-        $this->assertEquals($rfDevice->device_id, $foundRfDevice->device_id);
+        return $rfDevice;
     }
 }
