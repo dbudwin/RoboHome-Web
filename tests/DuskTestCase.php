7b5a13f9e12b097f788a1ce15ce2d173572711f8
Drew Budwin <drew@budw.in>
2017-08-11 13:16:16 -0400

    Installed Laravel Dusk
    Ran `php artisan dusk:install`.  There are some files here that will be removed/changed during construction of the actual tests.  Just including what it did by default.

---
+++ tests/DuskTestCase.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests;
+
+use Laravel\Dusk\TestCase as BaseTestCase;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+
+abstract class DuskTestCase extends BaseTestCase
+{
+    use CreatesApplication;
+
+    public static function prepare()
+    {
+        static::startChromeDriver();
+    }
+
+    protected function driver()
+    {
+        return RemoteWebDriver::create(
+            'http://localhost:9515',
+            DesiredCapabilities::chrome()
+        );
+    }
+}
