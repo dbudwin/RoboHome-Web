d04cae17eda551f096554b475dedac31f01f8098
Drew Budwin <drew@budw.in>
2017-08-11 13:19:17 -0400

    Added CreatesApplication trait
    See this for details about why/how this was done: https://github.com/laravel/dusk/issues/40

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,25 +3,16 @@
 namespace Tests;
 
 use Faker;
-use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected $baseUrl = 'http://localhost';
+    use CreatesApplication;
+
     protected static $faker;
 
     public static function setUpBeforeClass()
     {
         self::$faker = Faker\Factory::create();
-    }
-
-    public function createApplication()
-    {
-        $app = require __DIR__ . '/../bootstrap/app.php';
-
-        $app->make(Kernel::class)->bootstrap();
-
-        return $app;
     }
 }
