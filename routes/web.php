8632ccd49aef52640e57159764abc89bd01c62f0
Drew Budwin <drew@budw.in>
2017-08-18 16:07:33 -0400

    Added support for routing for EN, ES, JA languages
    The big `laravellocalization.php` file was created by following the README in the project:
    
    `php artisan vendor:publish --provider="Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider"`
    
    This makes a copy of the config file so it can be versioned.

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
-Route::get('/', 'Web\LoginController@index')->name('index');
-Route::get('/login', 'Web\LoginController@login')->name('login');
-Route::get('/logout', 'Web\LoginController@logout')->name('logout');
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ],
+    function () {
+        Route::get('/', 'Web\LoginController@index')->name('index');
+        Route::get('/login', 'Web\LoginController@login')->name('login');
+        Route::get('/logout', 'Web\LoginController@logout')->name('logout');
 
-Route::get('/devices', 'Web\DevicesController@devices')->name('devices');
-Route::post('/devices/add', 'Web\DevicesController@add')->name('addDevice');
-Route::get('/devices/delete/{id}', 'Web\DevicesController@delete')->name('deleteDevice');
-Route::post('/devices/{action}/{id}', 'Web\DevicesController@handleControlRequest')
-    ->where(['action' => '[a-z]+'])
-    ->name('handleControlRequest');
+        Route::get('/devices', 'Web\DevicesController@devices')->name('devices');
+        Route::post('/devices/add', 'Web\DevicesController@add')->name('addDevice');
+        Route::get('/devices/delete/{id}', 'Web\DevicesController@delete')->name('deleteDevice');
+        Route::post('/devices/{action}/{id}', 'Web\DevicesController@handleControlRequest')
+            ->where(['action' => '[a-z]+'])
+            ->name('handleControlRequest');
+    }
+);
