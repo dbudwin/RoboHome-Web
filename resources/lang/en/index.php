a49067f23f70803eb254b830fc766575834edfac
Drew Budwin <drew@budw.in>
2017-08-18 16:08:37 -0400

    Added English language templates

---
+++ resources/lang/en/index.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'page' => 'Login',
+    'welcome' => 'Welcome to RoboHome',
+    'message' => 'RoboHome is a SaaS tool that also integrates with Amazon\'s Echo to enable control of semi-connected devices (think IR, and RF) in your house over wifi!',
+    'loginButton' => 'Log In'
+];
