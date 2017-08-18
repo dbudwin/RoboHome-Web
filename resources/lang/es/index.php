500e4e8a7222c92885913da1f9f384f634f6cbbb
Drew Budwin <drew@budw.in>
2017-08-18 16:08:50 -0400

    Added Spanish language templates

---
+++ resources/lang/es/index.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'page' => 'Iniciar sesión',
+    'welcome' => 'Bienvenido a RoboHome',
+    'message' => '¡RoboHome es una herramienta ScuS que también se integra con el Echo de Amazon para habilitar el control de dispositivos semiconectados (como dispositivos con infrarrojo o frecuencias de radio) en su casa por medio del wifi!',
+    'loginButton' => 'Iniciar sesión'
+];
