5b639c51d99bd9b0c206c234ac630d4517499dc9
Drew Budwin <drew@budw.in>
2017-08-18 16:08:59 -0400

    Added Japanese language templates

---
+++ resources/lang/ja/global.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'app' => 'RoboHome',
+    'logout' => 'ログアウト'
+];
---
+++ resources/lang/ja/index.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'page' => 'ログイン',
+    'welcome' => 'RoboHomeへようこそ',
+    'message' => 'RoboHome is a SaaS tool that also integrates with Amazon\'s Echo to enable control of semi-connected devices (think IR, and RF) in your house over wifi!',
+    'loginButton' => 'ログイン'
+];
