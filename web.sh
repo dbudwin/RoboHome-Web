09233142d640f0d11221e6d57377ec562ca84a8b
Drew Budwin <drew@budw.in>
2018-02-28 22:29:06 -0500

    Update Code Climate and test settings
    Added some new linters for shell scripts.  Removed checks for static access for PHP Mess Detector since Laravel makes heavy usage of this functionality.  Corrected some settings that were affecting tests not running right.

--- web.sh
+++ web.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 project="${PWD##*/}"
-project=$(echo $project | tr '[:upper:]' '[:lower:]' | sed -e "s/-//g")
+project=$(echo "$project" | tr '[:upper:]' '[:lower:]' | sed -e "s/-//g")
 
-eval "docker exec -it ${project}_web_1 $@"
+eval "docker exec -it ${project}_web_1 $*"
