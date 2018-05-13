f26002c6742ce98d74d82bdf9768fccb533099af
Drew Budwin <drew@budw.in>
2018-05-13 14:58:46 -0400

    Use new Laravel 5.6 CSRF Blade directive
    Updated `{{ csrf_field() }}` to be `@csrf`.

--- resources/views/devices.blade.php
+++ resources/views/devices.blade.php
@@ -58,9 +58,8 @@
                                     <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                         Logout
                                     </a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
+                                        @csrf
                                     </form>
                                 </li>
                             </ul>
